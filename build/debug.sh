a1307911e8e0d29ab81f2a032021a9940ad36b3b
Max Rozdobudko <max.rozdobudko@gmail.com>
2014-10-17 10:45:47 +0300

    Adds build artifacts

--- build/debug.sh
+++ build/debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cp -R ../image-picker-air/image-picker-debug/bin-debug/ImagePickerDebug-app.xml launch/ImagePickerDebug-app.xml
-cp -R ../image-picker-air/image-picker-debug/bin-debug/ImagePickerDebug.swf launch/IMagePickerDebug.swf
+cp -R ../image-picker-air/image-picker-debug/bin-debug/ImagePickerDebug.swf launch/ImagePickerDebug.swf
 
 adt -package -target ipa-debug-interpreter -provisioning-profile $IOS_PROVISION -storetype pkcs12 -keystore $IOS_CERTIFICATE -storepass $IOS_CERTIFICATE_STOREPASS launch/ImagePickerDebug.ipa launch/ImagePickerDebug-app.xml -C launch ImagePickerDebug.swf -extdir launch/ext -platformsdk /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/
