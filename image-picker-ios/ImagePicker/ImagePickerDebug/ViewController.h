85ddbf6b8f6570085b40521d009c4e46f90f4fed
Max Rozdobudko <max.rozdobudko@gmail.com>
2014-10-17 10:44:33 +0300

    Adds testing projects

---
+++ image-picker-ios/ImagePicker/ImagePickerDebug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImagePickerDebug
+//
+//  Created by Maxim on 10/16/14.
+//  Copyright (c) 2014 Max Rozdobudko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
